d87957e358a483394c5d5a5251c876c066abc034
Paolo Innocenti <innocentipaolo1@gmail.com>
2019-12-27 12:40:51 +0100

    refactoring

--- controller.py
+++ controller.py
@@ -19,6 +19,28 @@ class Controller:
         self.window = BeautyWindow(None)
         self.connectButtons()
 
+    def connectButtons(self):
+        self.window.ui.rotateCWButton.clicked.connect(lambda: self.window.ui.imageWidget.rotate(90))
+        self.window.ui.rotateAntiCWButton.clicked.connect(lambda: self.window.ui.imageWidget.rotate(-90))
+        self.window.ui.loadPreviousButton.clicked.connect(self.previousPhoto)
+        self.window.ui.loadNextButton.clicked.connect(self.nextPhoto)
+        self.window.ui.loadButton.clicked.connect(lambda: self.loadFiles(self.window.ui.folderPathInput.text()))
+        self.window.ui.loadPreviousButton.setShortcut(QtCore.Qt.Key_Left)
+        self.window.ui.loadNextButton.setShortcut(QtCore.Qt.Key_Right)
+        self.window.ui.folderPathInput.returnPressed.connect(lambda: self.loadFiles(self.window.ui.folderPathInput.text()))
+
+    def previousPhoto(self):
+        if self.currentImageIndex > 0:
+            self.currentImageIndex -= 1
+            self.window.ui.imageWidget.loadImage(self.fileList[self.currentImageIndex])
+            self.loadExif()
+
+    def nextPhoto(self):
+        if self.currentImageIndex < len(self.fileList) - 1:
+            self.currentImageIndex += 1
+            self.window.ui.imageWidget.loadImage(self.fileList[self.currentImageIndex])
+            self.loadExif()
+
     def loadFiles(self, folderPath):
         try:
             os.chdir(folderPath)
@@ -27,7 +49,7 @@ class Controller:
             self.window.ui.folderNotFound.setStyleSheet("color:red;")
         else:
             self.fileList = []
-            types = ('*.jpg', '*.JPEG', '*.JPG', '*.png', '*.jpeg')
+            types = ( '*.jpeg', '*.jpg', '*.JPEG', '*.JPG', '*.png')
             for files in types:
                 self.fileList.extend(glob.glob(files))
             for i in range(0, len(self.fileList)):
@@ -36,21 +58,7 @@ class Controller:
             self.window.ui.imageWidget.loadImage(self.fileList[0])
             self.currentImageIndex = 0
             self.loadExif()
-            if self.window.ui.imageWidget.image.isPortrait():
-                reductionRate = 100
-                while self.window.ui.imageWidget.image.width() > 512:
-                    if self.window.ui.imageWidget.image.width() - reductionRate < 512:
-                        reductionRate = reductionRate / 2
-                    else:
-                        self.window.resize(self.window.width() - reductionRate, self.window.height())
-            else:
-                reductionRate = 100
-                while self.window.ui.imageWidget.image.height() > 512:
-                    if self.window.ui.imageWidget.image.height() - reductionRate < 512:
-                        reductionRate = reductionRate / 2
-                    else:
-                        self.window.resize(self.window.width(), self.window.height() - reductionRate)
-            print("Image size:", self.window.ui.imageWidget.image.width(), self.window.ui.imageWidget.image.height())
+            self.init512()
 
     def loadExif(self):
         exifData = self.window.ui.imageWidget.image.convertExif()
@@ -79,27 +87,29 @@ class Controller:
         table.setItem(table.rowCount() - 1, 0, QTableWidgetItem('GPS Info'))
         table.setCellWidget(table.rowCount() - 1, 1, gpsLabel)
 
-    def connectButtons(self):
-        self.window.ui.rotateCWButton.clicked.connect(lambda: self.window.ui.imageWidget.rotate(90))
-        self.window.ui.rotateAntiCWButton.clicked.connect(lambda: self.window.ui.imageWidget.rotate(-90))
-        self.window.ui.loadPreviousButton.clicked.connect(self.previousPhoto)
-        self.window.ui.loadNextButton.clicked.connect(self.nextPhoto)
-        self.window.ui.loadButton.clicked.connect(lambda: self.loadFiles(self.window.ui.folderPathInput.text()))
-        self.window.ui.loadPreviousButton.setShortcut(QtCore.Qt.Key_Left)
-        self.window.ui.loadNextButton.setShortcut(QtCore.Qt.Key_Right)
-        self.window.ui.folderPathInput.returnPressed.connect(lambda: self.loadFiles(self.window.ui.folderPathInput.text()))
-
-    def previousPhoto(self):
-        if self.currentImageIndex > 0:
-            self.currentImageIndex -= 1
-            self.window.ui.imageWidget.loadImage(self.fileList[self.currentImageIndex])
-            self.loadExif()
-
-    def nextPhoto(self):
-        if self.currentImageIndex < len(self.fileList)-1:
-            self.currentImageIndex += 1
-            self.window.ui.imageWidget.loadImage(self.fileList[self.currentImageIndex])
-            self.loadExif()
+    #only way to init the size of the picture to 512px
+    def init512(self):
+        if self.window.ui.imageWidget.image.isPortrait():
+            reductionRate = 100
+            while self.window.ui.imageWidget.image.width() != 512:
+                if self.window.ui.imageWidget.image.width() > 512:
+                    if self.window.ui.imageWidget.image.width() - reductionRate < 512:
+                        reductionRate = reductionRate / 2
+                    else:
+                        self.window.resize(self.window.width() - reductionRate, self.window.height())
+                else:
+                    self.window.resize(self.window.width() + 200, self.window.height())
+        else:
+            reductionRate = 100
+            while self.window.ui.imageWidget.image.height() != 512:
+                if self.window.ui.imageWidget.image.height() > 512:
+                    if self.window.ui.imageWidget.image.height() - reductionRate < 512:
+                        reductionRate = reductionRate / 2
+                    else:
+                        self.window.resize(self.window.width(), self.window.height() - reductionRate)
+                else:
+                    self.window.resize(self.window.width(), self.window.height() + 200)
+        print("Image size:", self.window.ui.imageWidget.image.imageWidth, self.window.ui.imageWidget.image.imageHeight)
 
     def run(self):
         self.window.show()
